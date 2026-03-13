74ef31f23eb2fe8e2dd9c2467141b70333038369
Ibrahim Emre Yildiz <iemreis803@gmail.com>
2026-03-13 14:38:07 +0300

    models.py dosyası

---
+++ models.py
@@ -0,0 +1,41 @@
+class Item:
+    def __init__(self,id,title):
+        self.id=id
+        self.title=title
+        
+    
+
+
+class Book(Item):
+    def __init__(self,id,title,author,is_borrowed=False):
+        self.author=author
+        self.is_borrowed=False
+        super().__init__(id,title)
+
+
+    def borrow(self):
+        if self.is_borrowed:
+            return f"{self.id} id'li {self.title} başlıklı kitap zaten ödünç alınmış."
+        else:
+            self.is_borrowed=True
+            return f"{self.id} id'li {self.title} başlıklı kitap ödünç alındı"
+        
+
+    def return_book(self):
+        if self.is_borrowed:
+            self.is_borrowed=False
+            return f"{self.id} id'li {self.title} başlıklı kitap iade edildi."
+        else:
+            return f"{self.id} id'li {self.title} başlıklı kitap ödünç alınmadığı için iade edilemez."
+    
+
+    def __str__(self):
+        if self.is_borrowed:
+            situation="Ödünç Alındı"
+        else:
+            situation="Rafta"
+        return f"{self.id}-{self.title}-{self.author}-{situation}"
+
+
+    def __repr__(self):
+        return f"Book(id='{self.id}', title='{self.title}', author='{self.author}')"
